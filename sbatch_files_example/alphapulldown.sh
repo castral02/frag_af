ea040eafaed68550a35e9d9932b3959f9b9cd353
castral02 <alex.castroverde@nih.gov>
2025-04-14 13:24:26 -0400

    Update alphapulldown.sh

--- sbatch_files_example/alphapulldown.sh
+++ sbatch_files_example/alphapulldown.sh
@@ -14,7 +14,7 @@ set -e
 set -x  # Enable debugging
 
 # Load necessary modules
-module load colabfold alphapulldown/0.30.7
+module load colabfold alphapulldown/1.0.4
 
 # Change to the input directory
 cd "/data/CBLCCBR/af_new/Mdm4"
